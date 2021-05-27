d91558896bfb20d98aba61ecaeba81998c37f021
yikwill <74379157+yikwill@users.noreply.github.com>
2021-05-27 17:58:29 -0600

    Update multvarbiastest.py

--- multvarbiastest.py
+++ multvarbiastest.py
@@ -1,18 +1,27 @@
 import math
 
+# Test inputs for a hiring process
 test_data = 20*[('male', 'white')] + [('female', 'white'), ('male', 'black'), ('female', 'asian')]
 test_numvars = 2
 test_varpossiblevals = [2, 3]
+test_assumed_bias = ('male', 'white')
 test_alpha = 0.05
 
-def multvarbiastest(data, numvars, varpossiblevals, alpha):
+# Test inputs for rolling a die 20 times
+test_data2 = 15*[(1)] + [(2), (3), (4), (5), (6)]
+test_numvars2 = 1
+test_varpossiblevals2 = [6]
+test_assumed_bias2 = (1)
+test_alpha2 = 0.05
+
+def multvarbiastest(data, numvars, varpossiblevals, assumed_bias, alpha):
     """
     Notes:
     1. data should be a list of tuples, not lists
     2. varpossiblevalues is a list of possible values for each variable
        in the order in which they appear in the tuples of data
     """
-    
+
     # Computes all possible combinations for a given data point
     possible_combos = 1
     for i in range(numvars):
@@ -23,6 +32,7 @@ def multvarbiastest(data, numvars, varpossiblevals, alpha):
     r = norm_scriptx*(1+math.log(norm_scriptx))
 
     # Assumed bias is the most common data entry
+    """
     freq_dict = {}
     for entry in data:
         if entry in freq_dict:
@@ -30,9 +40,11 @@ def multvarbiastest(data, numvars, varpossiblevals, alpha):
         else:
             freq_dict[entry] = 1
     assumed_bias = max(freq_dict, key=freq_dict.get)
+    """
+    bias = assumed_bias
     
     mg = 0
-    for i in range(0, len(data)-data.count(assumed_bias)+1):
+    for i in range(0, len(data)-data.count(bias)+1):
         mg += possible_combos * math.comb(len(data), i) * (possible_combos-1)**i
     
     nu = norm_scriptx/mg
