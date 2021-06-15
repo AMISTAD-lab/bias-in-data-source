d92f2e9e2aa13f96841a5a8aabce1a49b8f1788d
magpyed <78130750+magpyed@users.noreply.github.com>
2021-06-15 12:08:30 -0700

    Add files via upload

---
+++ data_binarizer.py
@@ -0,0 +1,10 @@
+
+
+def data_binarizer(nbdata, nbhypothesis, nbvaluelist, selectedvalue):
+    s_index = nbvaluelist.index(selectedvalue)
+    bvaluelist = [str(selectedvalue), 'not-'+str(selectedvalue)]
+    bhypothesis = [nbhypothesis[s_index], sum(nbhypothesis[:s_index]+nbhypothesis[s_index+1:])]
+    bdata = [str(selectedvalue)]*nbdata.count(selectedvalue) 
+    bdata += ['not-'+str(selectedvalue)]*(len(nbdata)-nbdata.count(selectedvalue))
+    bcounts = [nbdata.count(selectedvalue), len(nbdata)-nbdata.count(selectedvalue)]
+    return (bdata, bcounts, bhypothesis, bvaluelist)
