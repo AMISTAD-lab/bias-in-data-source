9c6c83d0d3847fb47e0159a887d4be5ff936cfd6
yikwill <74379157+yikwill@users.noreply.github.com>
2021-06-15 14:50:15 -0600

    Update q_finder_count_based.py

--- q_finder_count_based.py
+++ q_finder_count_based.py
@@ -1,10 +1,7 @@
-import decimal
 import math
 import numpy as np
 from scipy.optimize import minimize, LinearConstraint, NonlinearConstraint, Bounds
 from scipy.special import rel_entr
-from statistics import *
-from sympy import Symbol, solveset, S
 import warnings
 
 # Used to suppress useless Scipy warnings
@@ -14,10 +11,10 @@ warnings.filterwarnings("ignore")
 def q_finder_slsqp(observation, value_list, hypothesis, p_lowerbound):
     # Proposed distribution, p
     p = np.array(hypothesis)
-    value_count = {}
-    for value in value_list:
-        value_count[value] = observation.count(value)
-    observation_count = [observation.count(value) for value in value_list]
+    count_vector = [observation.count(value) for value in value_list]
+    # These are for calculating the multinomial PMF
+    n_fac = math.factorial(len(observation))
+    prod_x_fac = math.prod([math.factorial(x) for x in count_vector])
     # Defines loss function to be the KL divergence of Q from P
     def objective(q):
         return sum(rel_entr(q, p))
@@ -26,39 +23,15 @@ def q_finder_slsqp(observation, value_list, hypothesis, p_lowerbound):
         return sum(q)-1
     # Q(x) >= p_lowerbound = s_lowerbound*u(x)
     def nonlin_cons(q):
-        n_fac = math.factorial(len(observation))
-        q_to_count_list = [q[i]**value_count[value_list[i]] for i in range(len(value_list))]
-        return math.prod(q_to_count_list) - p_lowerbound
+        # This is q0^x0 * q1^x1 * ... * qn^xn
+        prod_q = math.prod([q[i]**count_vector[i] for i in range(len(count_vector))])
+        return n_fac*prod_q/prod_x_fac - p_lowerbound
     cons1 = ({'type': 'eq', 'fun': lin_cons})
     cons2 = ({'type': 'ineq', 'fun': nonlin_cons})
     # Each probability in Q must be between 0 and 1 inclusive
-    bounds = Bounds(len(value_list)*[0], len(value_list)*[1])
+    bounds = Bounds(len(count_vector)*[0], len(count_vector)*[1])
     # Bogus initial guess for the algorithm to start with
     x0 = np.array(len(value_list)*[1])
     res = minimize(objective, x0, method='SLSQP', constraints=[cons1, cons2], bounds=bounds, 
                    options={'maxiter': 1000000, 'ftol': 1e-100000, 'eps': 1.5e-7, 'disp': False})
-    return res.x
-
-def q_finder_trust_constr(observation, value_list, hypothesis, p_lowerbound):
-    # Uses original hypothesis as proposed distribution P
-    p = np.array(hypothesis)
-    value_count = {}
-    for value in value_list:
-        value_count[value] = observation.count(value)
-    # Defines loss function to be the KL divergence of Q from P
-    def objective(q):
-        return sum(rel_entr(q, p))
-    # The sum of all probabilities in Q must be 1
-    linear_constraint = LinearConstraint(len(value_list)*[1], [1], [1], keep_feasible=False)
-    # Q(x) >= p_lowerbound = s_lowerbound*u(x)
-    def nonlin_cons(q):
-        q_to_count_list = [q[i]**value_count[value_list[i]] for i in range(len(value_list))]
-        return math.prod(q_to_count_list)
-    nonlinear_constraint = NonlinearConstraint(nonlin_cons, lb=p_lowerbound, ub=np.inf, keep_feasible=True)
-    # Each probability in Q must be between 0 and 1 inclusive
-    bounds = Bounds(len(value_list)*[0], len(value_list)*[1], keep_feasible=True)
-    # Bogus initial guess for the algorithm to start with
-    x0 = np.array(len(value_list)*[1])
-    res = minimize(objective, x0, method='trust-constr', constraints=[linear_constraint, nonlinear_constraint], 
-                   options={'gtol': 1e-100, 'xtol': 1e-100, 'maxiter': 1000000, 'verbose': 0}, bounds=bounds)
     return res.x
