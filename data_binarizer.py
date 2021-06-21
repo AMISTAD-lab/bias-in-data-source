f5e2e172755fb62ca429e360b4b0e54e0f34cd7d
yikwill <74379157+yikwill@users.noreply.github.com>
2021-06-21 16:10:07 -0600

    Made data_binarizer use fsum instead of sum to maintain floating point accuracy

--- data_binarizer.py
+++ data_binarizer.py
@@ -1,9 +1,9 @@
-
+import math
 
 def data_binarizer(nbdata, nbhypothesis, nbvaluelist, selectedvalue):
     s_index = nbvaluelist.index(selectedvalue)
     bvaluelist = [str(selectedvalue), 'not-'+str(selectedvalue)]
-    bhypothesis = [nbhypothesis[s_index], sum(nbhypothesis[:s_index]+nbhypothesis[s_index+1:])]
+    bhypothesis = [nbhypothesis[s_index], math.fsum(nbhypothesis[:s_index]+nbhypothesis[s_index+1:])]
     bdata = [str(selectedvalue)]*nbdata.count(selectedvalue) 
     bdata += ['not-'+str(selectedvalue)]*(len(nbdata)-nbdata.count(selectedvalue))
     bcounts = [nbdata.count(selectedvalue), len(nbdata)-nbdata.count(selectedvalue)]
