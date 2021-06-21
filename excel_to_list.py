2ca7ff6c4cad5f93621c9f3c49c541ae9bc21f93
timothylindsey <84877060+timothylindsey@users.noreply.github.com>
2021-06-21 09:54:31 -0700

    Update excel_to_list.py

--- excel_to_list.py
+++ excel_to_list.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+#filepath must be in format, r"(filepath)", ex. filepath = r"C:\Users\Tim\Downloads\HR_Dataset.xlsx"
+
 #gets all the columns/classes from the excel spreadshet
 def excel_to_list(filepath):
     data = pd.read_excel(filepath) 
@@ -7,7 +9,8 @@ def excel_to_list(filepath):
     array = df.to_numpy()
     data_list = array.tolist()
     return data_list
-	
+
+#columns must be in format of list, ex. columns= ['Sex','GenderID', 'RaceDesc','EmploymentStatus']
 #gets only the columns/classes you want from the excel spreadsheet
 def excel_to_list(filepath,columns):
     data = pd.read_excel(filepath) 
