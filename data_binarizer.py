c1fa1ffc0d62ca1c38a7e0d8f7e2ad3323ccb996
magpyed <78130750+magpyed@users.noreply.github.com>
2021-06-22 10:54:19 -0700

    Add files via upload

--- data_binarizer.py
+++ data_binarizer.py
@@ -8,3 +8,19 @@ def data_binarizer(nbdata, nbhypothesis, nbvaluelist, selectedvalue):
     bdata += ['not-'+str(selectedvalue)]*(len(nbdata)-nbdata.count(selectedvalue))
     bcounts = [nbdata.count(selectedvalue), len(nbdata)-nbdata.count(selectedvalue)]
     return (bdata, bcounts, bhypothesis, bvaluelist)
+
+
+def data_binarizer_multival(nbdata, nbhypothesis, nbvaluelist, selectedvaluelist):
+    #probably a better way to get the name but. it's not coming to me rn
+    biasval_name = '('+ str(selectedvaluelist[0])
+    for val in selectedvaluelist[1:]:
+        biasval_name += ", " + str(val)
+    biasval_name+=')'
+    bvaluelist = [biasval_name, 'not-'+biasval_name]
+    num_biasval = sum([nbdata.count(x) for x in selectedvaluelist])
+    bcounts = [num_biasval, len(nbdata)-num_biasval]
+    bdata = [biasval_name]*num_biasval + ['not-'+biasval_name]*(len(nbdata)-num_biasval)
+    bhypothesis = [math.fsum([nbhypothesis[nbvaluelist.index(x)] for x in selectedvaluelist])]
+    bhypothesis += [1-bhypothesis[0]]
+    return (bdata,bcounts,bhypothesis,bvaluelist)
+    
