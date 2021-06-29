6c905ba48806e9ecf7a422d4c374d4b17fcd39d8
yikwill <74379157+yikwill@users.noreply.github.com>
2021-06-29 12:55:55 -0600

    Update q_finder_count_based.py

--- q_finder_count_based.py
+++ q_finder_count_based.py
@@ -37,7 +37,7 @@ def binary_q_finder_main(binary_count_vector, hypothesis, alpha):
     p = np.array(hypothesis)
     n = sum(binary_count_vector)
     k = binary_count_vector[0]
-    constraint_constant = math.log(alpha * (n+1)**(-1) * math.comb(n, k)**(-1))
+    constraint_constant = math.log(alpha * mpf(n+1)**(-1) * mpf(math.comb(n, k))**(-1))
     def objective(q):
         return sum(rel_entr(q, p))
     linear_constraint = LinearConstraint([1,1], [1], [1], keep_feasible=False)
