12b4e6c5218cafb23a5d9dea64b854f3af254f12
yikwill <74379157+yikwill@users.noreply.github.com>
2021-06-29 12:19:59 -0600

    Update hypothesis_test.py

--- hypothesis_test.py
+++ hypothesis_test.py
@@ -1,6 +1,7 @@
 from q_finder_count_based import *
 from counts_kardis_test import *
 from s_prime import *
+from data_binarizer import *
 
 def hypothesis_test(data, value_list, alpha = 0.05, hypothesis = []):
     if hypothesis == []:
@@ -16,6 +17,25 @@ def hypothesis_test(data, value_list, alpha = 0.05, hypothesis = []):
         print("Any plausible distribution must boost probability over the given distribution by " \
             + str(s_lowerbound) + ", and will therefore have a minimum probability of " + str(p_lowerbound) + ".")
         q = list(q_finder_main(count_vector, hyp, p_lowerbound))
+        print("Closest plausible distribution: " + str(q))
+        return (kardis, reject, s_lowerbound, p_lowerbound, q)
+    else:
+        print("Proposed distribution not rejected at alpha = " + str(alpha)  + ". Kardis = " + str(kardis) + ".")
+        return (kardis, reject)
+
+def binary_hypothesis_test(data, value_list, selected_value_list, alpha=0.05, binary_hypothesis=[0.5,0.5]):
+    count_vector = [data.count(x) for x in value_list]
+    binary_count_vector = data_binarizer_main(count_vector, value_list, selected_value_list)
+    n = sum(binary_count_vector)
+    k = binary_count_vector[0]
+    kardis, reject, r, nu, h = kardis_test_main(binary_count_vector, alpha, binary_hypothesis)
+    if reject:
+        print("Proposed distribution rejected at alpha = " + str(alpha)  + ". Kardis = " + str(kardis) + ".")
+        s_lowerbound = alpha * ((n+1) * math.comb(n,k) * binary_hypothesis[0]**k * binary_hypothesis[1]**(n-k))**(-1)
+        p_lowerbound = s_lowerbound*h
+        print("Any plausible distribution must boost probability over the given distribution by " \
+            + str(s_lowerbound) + ", and will therefore have a minimum probability of " + str(p_lowerbound) + ".")
+        q = list(binary_q_finder_main(binary_count_vector, binary_hypothesis, alpha))
         print("Closest plausible distribution: " + str(q))
         return (kardis, reject, s_lowerbound, p_lowerbound, q)
     else:
