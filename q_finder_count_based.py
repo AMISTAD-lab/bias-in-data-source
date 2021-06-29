9c2eee538a4f8d10e4c3c300f983d54314b2b20a
yikwill <74379157+yikwill@users.noreply.github.com>
2021-06-29 12:07:17 -0600

    Implemented binary_hypothesis_test

--- q_finder_count_based.py
+++ q_finder_count_based.py
@@ -9,33 +9,6 @@ import warnings
 # Disable this if you're rigorously testing
 warnings.filterwarnings("ignore")
 
-def q_finder_trust_constr(observation, value_list, hypothesis, p_lowerbound):
-    # Uses original hypothesis as proposed distribution P
-    p = np.array(hypothesis)
-    count_vector = [observation.count(x) for x in value_list]
-    # Log scales certain parts of the constant parts of the pmf
-    log_n_fac = math.log(math.factorial(len(observation)))
-    log_prod_x_fac = math.log(math.prod([math.factorial(x) for x in count_vector]))
-    constraint_constant = math.log(p_lowerbound) - log_n_fac + log_prod_x_fac
-    # Defines loss function to be the KL divergence of Q from P
-    def objective(q):
-        return sum(rel_entr(q, p))
-    # The sum of all probabilities in Q must be 1
-    linear_constraint = LinearConstraint(len(value_list)*[1], [1], [1], keep_feasible=False)
-    # log(Q(x)) - constraint_constant >= 0
-    def nonlin_cons(q):
-        log_prod_q = sum([count_vector[i]*math.log(q[i]) for i in range(len(count_vector))])
-        constraint = log_prod_q - constraint_constant
-        return constraint
-    nonlinear_constraint = NonlinearConstraint(nonlin_cons, lb=0, ub=np.inf, keep_feasible=True)
-    # Each probability in Q must be between 0 and 1 inclusive
-    bounds = Bounds(len(value_list)*[0], len(value_list)*[1], keep_feasible=True)
-    # Bogus initial guess for the algorithm to start with
-    x0 = np.array(len(value_list)*[1])
-    res = minimize(objective, x0, method='trust-constr', constraints=[linear_constraint, nonlinear_constraint], 
-                   options={'gtol': 1e-100, 'xtol': 1e-100, 'maxiter': 1000000, 'verbose': 0}, bounds=bounds)
-    return res.x
-
 def q_finder_main(count_vector, hypothesis, p_lowerbound):
     p = np.array(hypothesis)
     log_n_fac = math.log(math.factorial(sum(count_vector)))
@@ -56,6 +29,23 @@ def q_finder_main(count_vector, hypothesis, p_lowerbound):
     bounds = Bounds(len(count_vector)*[0], len(count_vector)*[1], keep_feasible=True)
     # Bogus initial guess for the algorithm to start with
     x0 = np.array(len(count_vector)*[0.99])
+    res = minimize(objective, x0, method='trust-constr', constraints=[linear_constraint, nonlinear_constraint], 
+                   options={'gtol': 1e-100, 'xtol': 1e-100, 'maxiter': 1000000, 'verbose': 0}, bounds=bounds)
+    return res.x
+
+def binary_q_finder_main(binary_count_vector, hypothesis, alpha):
+    p = np.array(hypothesis)
+    n = sum(binary_count_vector)
+    k = binary_count_vector[0]
+    constraint_constant = math.log(alpha * (n+1)**(-1) * math.comb(n, k)**(-1))
+    def objective(q):
+        return sum(rel_entr(q, p))
+    linear_constraint = LinearConstraint([1,1], [1], [1], keep_feasible=False)
+    def nonlin_cons(q):
+        return k*math.log(q[0]) + (n-k)*math.log(q[1]) - constraint_constant
+    nonlinear_constraint = NonlinearConstraint(nonlin_cons, lb=0, ub=np.inf, keep_feasible=True)
+    bounds = Bounds([0,0], [1,1], keep_feasible=True)
+    x0 = np.array([0.99,0.99])
     res = minimize(objective, x0, method='trust-constr', constraints=[linear_constraint, nonlinear_constraint], 
                    options={'gtol': 1e-100, 'xtol': 1e-100, 'maxiter': 1000000, 'verbose': 0}, bounds=bounds)
     return res.x
