d20290e892ee130434adc027da0b814569e3cb7a
magpyed <78130750+magpyed@users.noreply.github.com>
2021-07-01 13:20:28 -0700

    Update excel_to_list.py

--- excel_to_list.py
+++ excel_to_list.py
@@ -1,18 +1,21 @@
 import pandas as pd
 
-#filepath must be in format, r"(filepath)", ex. filepath = r"C:\Users\Tim\Downloads\HR_Dataset.xlsx"
-
-#gets all the columns/classes from the excel spreadshet
 def excel_to_list(filepath):
+    """
+    Acquires all of the columns/classes from the excel spreadsheet found at 'filepath'.
+    'filepath' must be in the format r"(filepath)", ex. filepath = r"C:\Users\Tim\Downloads\HR_Dataset.xlsx"
+    """
     data = pd.read_excel(filepath) 
     df = pd.DataFrame(data)
     array = df.to_numpy()
     data_list = array.tolist()
     return data_list
 
-#columns must be in format of list, ex. columns= ['Sex','GenderID', 'RaceDesc','EmploymentStatus']
-#gets only the columns/classes you want from the excel spreadsheet
 def excel_to_list(filepath,columns):
+    """
+    Acquires only the requested columns/classes from the excel spreadsheet found at 'filepath'.
+    'filepath' must be in the format described above. 'columns' is the list of requested columns.
+    """
     data = pd.read_excel(filepath) 
     df = pd.DataFrame(data,columns= col) #column names must be identical to the name given in the excel spreadsheet
     array = df.to_numpy()
