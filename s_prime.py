24e8b54877f560e3c04faed0cd58527638e770e0
magpyed <78130750+magpyed@users.noreply.github.com>
2021-07-01 13:15:15 -0700

    Update s_prime.py

--- s_prime.py
+++ s_prime.py
@@ -4,6 +4,17 @@ from data_binarizer import *
 
 
 def s_prime_finder_main(count_vector, value_list, selected_value_list, alpha, binary_hypothesis, sigfigs):
+    """
+    For the tightest-bound binary function. Finds the coefficient necessary 
+    on the selected value probability to produce a plausible distribution. 
+    'count_vector' is the original non-binary count representation of the data,
+    'value_list' contains all the values a data point may take, 
+    'selected_value_list' is the collection of values the user believes
+    the data is biased towards. 'alpha' is the requested alpha-level, 
+    'binary_hypothesis' contains the probability of producing a
+    selected value or a not-selected value,
+    and 'sigfigs' is the requested number of significant figures.
+    """
     binom_dict = {}
     flipped = False
     bcounts = data_binarizer_main(count_vector, value_list, selected_value_list)
@@ -12,8 +23,14 @@ def s_prime_finder_main(count_vector, value_list, selected_value_list, alpha, bi
     p_b = binary_hypothesis[0]
 
     def stirl_approx(k):
+            """
+            Approximates nCk using Stirling's approximation.
+            """
             return sqrt(n)*mpf(n)**mpf(n)/(sqrt(2*math.pi*k*(n-k))*mpf(k)**k*mpf(n-k)**(n-k))
     def binom(p):
+        """
+        Returns the binomial one-sided (greater) tail probability.
+        """
         if p in binom_dict.keys():
             return binom_dict[p]
         else:
@@ -33,6 +50,9 @@ def s_prime_finder_main(count_vector, value_list, selected_value_list, alpha, bi
     cur_val = 1/p_b
 
     def buddy(s):
+        """
+        Recursively optimizes s' for a single power of 10.
+        """
         lo = (s - 10**cur_pow10)
         lo_binom = binom(lo*p_b)
         if lo_binom < alpha:
