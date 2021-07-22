e4214a793b7d200641dce1ab5594e21866024bcd
timothylindsey <84877060+timothylindsey@users.noreply.github.com>
2021-07-22 11:21:41 -0700

    Update excel_to_list.py

--- excel_to_list.py
+++ excel_to_list.py
@@ -17,7 +17,7 @@ def excel_to_list_2(filepath,columns):
     'filepath' must be in the format described above. 'columns' is the list of requested columns.
     """
     data = pd.read_excel(filepath) 
-    df = pd.DataFrame(data,columns= col) #column names must be identical to the name given in the excel spreadsheet
+    df = pd.DataFrame(data,columns= columns) #column names must be identical to the name given in the excel spreadsheet
     array = df.to_numpy()
     data_list = array.tolist()
     return data_list
