c18fcdd0e14740435d789de83aa7bbe4d0589faa
yikwill <74379157+yikwill@users.noreply.github.com>
2021-07-28 11:19:02 -0600

    Update kardis_test.py

--- kardis_test.py
+++ kardis_test.py
@@ -25,29 +25,3 @@ def kardis_test_main(counts, alpha, hypothesis):
     else:
         reject = False
     return (kardis, reject, r, nu, h)
-
-def uniform_dist_kardis_test(observation, value_list, alpha):
-    """
-    Performs a multinomial kardis test using the uniform distribution.
-    'observation' is the list of data points, 'value_list' is a list of the values
-    a data point may take, and 'alpha' is the chosen alpha level. 
-    Returns the kardis value and a boolean representing rejection.
-    This function is largely deprecated.
-    """
-    obs_counts = [observation.count(x) for x in value_list]
-    num_bins = len(value_list)
-    norm_scriptx = math.comb(len(observation)+num_bins-1, num_bins-1)
-    u_hyp = [1/num_bins]*num_bins
-    u = mpf(math.factorial(len(observation))) / math.prod([mpf(math.factorial(x)) for x in obs_counts])\
-    * math.prod([mpf(u_hyp[x])**mpf(obs_counts[x]) for x in range(num_bins)])
-    mg = mg_calculator(obs_counts, u_hyp)
-    nu = norm_scriptx/mg
-    r = norm_scriptx*(1+math.log(norm_scriptx))
-    kardis = r*u/nu
-    if kardis < alpha:
-        reject = True
-        print("Uniform distribution rejected at alpha = "+ str(alpha) + ". p(x) = " + str(u) + ". Kardis = " + str(kardis))
-    else:
-        reject = False
-        print("Uniform distribution not rejected at alpha = "+ str(alpha)  + ". p(x) = " + str(u) +". Kardis = " + str(kardis))
-    return (kardis, reject)
